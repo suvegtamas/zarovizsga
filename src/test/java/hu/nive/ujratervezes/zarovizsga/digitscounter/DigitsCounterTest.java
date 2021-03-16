e26ff6da1f58dc992b878b26cc15a9fc1c16c430
suvegtamas <suvegtamas97@gmail.com>
2021-03-16 17:12:58 +0100

    First commit

---
+++ src/test/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounterTest.java
@@ -0,0 +1,21 @@
+package hu.nive.ujratervezes.zarovizsga.digitscounter;
+
+import hu.nive.ujratervezes.zarovizsga.digitscounter.DigitsCounter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DigitsCounterTest {
+
+    @Test
+    void getCountOfDigits() {
+        DigitsCounter digitsCounter = new DigitsCounter();
+
+        assertEquals(0, digitsCounter.getCountOfDigits(null));
+        assertEquals(0, digitsCounter.getCountOfDigits(""));
+        assertEquals(1, digitsCounter.getCountOfDigits("1"));
+        assertEquals(2, digitsCounter.getCountOfDigits("111112222"));
+        assertEquals(3, digitsCounter.getCountOfDigits("1212123123123123"));
+        assertEquals(3, digitsCounter.getCountOfDigits("12asd 12ewr1q2breg31 rw23123123"));
+    }
+}
---
+++ src/test/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHoursTest.java
@@ -0,0 +1,16 @@
+package hu.nive.ujratervezes.zarovizsga.workhours;
+
+import hu.nive.ujratervezes.zarovizsga.workhours.WorkHours;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkHoursTest {
+
+    @Test
+    void minWork() {
+        WorkHours workHours = new WorkHours();
+        String result = workHours.minWork("src/test/resources/hu/nive/ujratervezes/zarovizsga/workhours/workhours.txt");
+        assertEquals("John Doe: 2021-01-04", result);
+    }
+}
